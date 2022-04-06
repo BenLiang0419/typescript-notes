d6ebfb3447b63f5d9b5c63978a13e65e7f4a9b3f
liangjs <asd153200>
2022-04-06 18:27:49 +0800

    feat: 编写MyPick

--- src/type-challenges/Pick.ts
+++ src/type-challenges/Pick.ts
@@ -1,1 +1,36 @@
 
+// 题目
+/**
+
+    interface Todo {
+        title: string
+        description: string
+        completed: boolean
+    }
+
+    type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+    const todo: TodoPreview = {
+        title: 'Clean room',
+        completed: false,
+    }
+
+ */
+
+interface Todo {
+    title: string
+    description: string
+    completed: boolean
+}
+
+type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+const todo: TodoPreview = {
+    title: 'Clean room',
+    completed: false,
+}
+
+// 实现MyPick
+type MyPick<T, U extends keyof T> = {
+     [key in U]: T[key]
+} 
